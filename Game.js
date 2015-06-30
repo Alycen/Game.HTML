075f737de87315ec3c1cfccdeddbdc052c417b21
Alycen <alsunnid@gmail.com>
2015-06-30 21:41:43 +0100

    Initial base to the main file

--- Game.js
+++ Game.js
@@ -1,2 +1,64 @@
 var game;
-var MAINMENU = 0, LEVEL_ONE = 1, LEVEL_TWO;
+var SPLASH_SCREEN = 0, START_SCREEN = 1, LEVEL_ONE = 2, LEVEL_TWO = 3, MAIN_MENU = 4, END_SCREEN =5;
+var startScreen, level_1, level_2;
+
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+function Game() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+    this.scene = LEVEL_ONE;
+    //this.isMultiplayer = false;
+}
+
+function main() {
+    game = new Game();
+    game.touches = [];
+
+}
+
+Game.prototype.initCanvas = function() {
+
+}
+
+Game.prototype.gameLoop = function() {
+
+}
+
+Game.prototype.Draw = function() {
+
+}
+
+/*Game.prototype.setMultiplayer = function(state) {
+
+}*/
+
+
+
+
+function onTouchStart(e) {
+    e.preventDefault();
+    game.touches = e.touches;
+    console.log("touch start");
+}
+
+function onTouchMove(e) {
+    e.preventDefault();
+    game.touches = e.touches;
+    console.log("touch move");
+}
+
+function onTouchEnd(e) {
+    e.preventDefault();
+    game.touches = e.touches;
+    console.log("touch end");
+}
