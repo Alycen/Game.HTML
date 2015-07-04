00989df65138a0e4a6b2eece5b27f742fddf6d9b
alycen <alsunnid@gmail.com>
2015-07-04 16:31:39 +0100

    Added more bulk until I have an idea for a gaame :(

---
+++ Level.js
@@ -0,0 +1,18 @@
+function Level() {
+    this.x = 0;
+    this.y = 0;
+    this.width = game.screenWidth;
+    this.height = game.screenHeight;
+    this.scale = 1;
+    this.isVisible = false;
+    this.img = new Image();
+    this.img.src = "assets/gfx/game/level.png"
+}
+
+Level.prototype.Update = function() {
+
+}
+
+Level.prototype.Draw = function() {
+    
+}
